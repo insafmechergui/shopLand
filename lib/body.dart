745ceb218ad67ef9d2ab220a67e645474e422eba
insafmechergui <insaf_mechergui@outlook.com>
2021-09-23 19:28:39 +0100

    starting body

--- lib/body.dart
+++ lib/body.dart
@@ -12,7 +12,7 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     return Container(
       margin:EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-      child: Text('body'),
+      child: Text('Welcome to the store'),
     );
   }
 }
