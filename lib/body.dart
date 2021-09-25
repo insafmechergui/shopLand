db08af2458f08baf522e75056767722254305bff
insafmechergui <insaf_mechergui@outlook.com>
2021-09-25 11:47:50 +0100

    creating clickable containers

--- lib/body.dart
+++ lib/body.dart
@@ -16,65 +16,90 @@ class _BodyState extends State<Body> {
         spacing: 10.0,
         runSpacing: 20.0,
         children: [
-      Container(
-        width: 300,
-        height: 300,
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          color: Colors.deepOrange[900],
-        ),
-        transform: Matrix4.rotationZ(0.08),
-        padding: EdgeInsets.all(20.0),
-        margin: EdgeInsets.all(20.0),
-        child: Image.asset('images/fashion.png', fit: BoxFit.cover),
-      ),
-      Container(
-        width: 300,
-        height: 300,
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          color: Colors.deepOrange[900],
-        ),
-        padding: EdgeInsets.all(20.0),
-        margin: EdgeInsets.all(20.0),
-        child: Image.asset('images/women.png', fit: BoxFit.cover),
-      ),
-      Container(
-        width: 300,
-        height: 300,
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          color: Colors.deepOrange[900],
-        ),
-        transform: Matrix4.rotationZ(0.02),
-        padding: EdgeInsets.all(20.0),
-        margin: EdgeInsets.all(20.0),
-        child: Image.asset('images/kids.png', fit: BoxFit.cover),
-      ),
-      Container(
-        width: 300,
-        height: 300,
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          color: Colors.deepOrange[900],
-        ),
-        transform: Matrix4.rotationZ(0.02),
-        padding: EdgeInsets.all(20.0),
-        margin: EdgeInsets.all(20.0),
-        child: Image.asset('images/kids.png', fit: BoxFit.cover),
-      ),
-      Container(
-        width: 300,
-        height: 300,
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          color: Colors.deepOrange[900],
-        ),
-        transform: Matrix4.rotationZ(0.02),
-        padding: EdgeInsets.all(20.0),
-        margin: EdgeInsets.all(20.0),
-        child: Image.asset('images/kids.png', fit: BoxFit.cover),
-      ),
-    ]);
+          InkWell(
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.deepOrange[900],
+              ),
+              transform: Matrix4.rotationZ(0.08),
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(20.0),
+              child: Image.asset('images/fashion.png', fit: BoxFit.cover),
+            ),
+            onTap: () {
+              print("Tapped on container");
+            },
+          ),
+          InkWell(
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.deepOrange[900],
+              ),
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(20.0),
+              child: Image.asset('images/women.png', fit: BoxFit.cover),
+            ),
+            onTap: () {
+              print("Tapped on container2");
+            },
+          ),
+          InkWell(
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.deepOrange[900],
+              ),
+              transform: Matrix4.rotationZ(0.02),
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(20.0),
+              child: Image.asset('images/kids.png', fit: BoxFit.cover),
+            ),
+            onTap: () {
+              print("Tapped on container3");
+            },
+          ),
+          InkWell(
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.deepOrange[900],
+              ),
+              transform: Matrix4.rotationZ(0.02),
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(20.0),
+              child: Image.asset('images/kids.png', fit: BoxFit.cover),
+            ),
+            onTap: () {
+              print("Tapped on container4");
+            },
+          ),
+          InkWell(
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.deepOrange[900],
+              ),
+              transform: Matrix4.rotationZ(0.02),
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(20.0),
+              child: Image.asset('images/kids.png', fit: BoxFit.cover),
+            ),
+            onTap: () {
+              print("Tapped on container5");
+            },
+          ),
+        ]);
   }
 }
